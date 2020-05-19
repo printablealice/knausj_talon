6c3f94d7aa55ce6b76192a68e508c1706b0c40da
Aaron Adams <aaron.adams@nccgroup.com>
2020-05-19 12:17:53 +0800

    black fixer pass

--- code/taskwarrior.py
+++ code/taskwarrior.py
@@ -1,69 +1,75 @@
+import sys
 from typing import Set
 
-from talon import Module, Context, actions
-import sys
+from talon import Context, Module, actions
 
 mod = Module()
 ctx = Context()
 
-ctx.lists['self.task_unmodifiable_verbs'] = {
-        "version" : "--version",
-        "help" : "help",
-        "commands" : "commands",
+ctx.lists["self.task_unmodifiable_verbs"] = {
+    "version": "--version",
+    "help": "help",
+    "commands": "commands",
 }
 
-ctx.lists['self.task_modifiable_verbs'] = {
-        "next" : "next",
-        "edit" : "edit",
-        "list" : "list",
-        "add" : "add",
-        "delete" : "delete",
-        "done" : "done",
+ctx.lists["self.task_modifiable_verbs"] = {
+    "next": "next",
+    "edit": "edit",
+    "list": "list",
+    "add": "add",
+    "delete": "delete",
+    "done": "done",
 }
 
-ctx.lists['self.task_filters'] = {
+ctx.lists["self.task_filters"] = {}
+
+ctx.lists["self.task_modifiers"] = {
+    "orphans": "project:",
+    "untagged": "tags.none:",
+    "high priority": "priority:H",
+    "medium priority": "priority:M",
+    "low priority": "priority:L",
 }
 
-ctx.lists['self.task_modifiers'] = {
-        "orphans" : "project:",
-        "untagged" : "tags.none:",
-        "high priority" : "priority:H",
-        "medium priority" : "priority:M",
-        "low priority" : "priority:L",
-}
+mod.list("task_unmodifiable_verbs", desc="taskwarrior unmodifiable commands")
+mod.list("task_modifiable_verbs", desc="taskwarrior modifiable commands")
+mod.list("task_modifiers", desc="taskwarrior command modifiers")
 
-mod.list('task_unmodifiable_verbs', desc='taskwarrior unmodifiable commands')
-mod.list('task_modifiable_verbs',   desc='taskwarrior modifiable commands')
-mod.list('task_modifiers',          desc='taskwarrior command modifiers')
 
 @mod.capture
 def task_unmodifiable_verbs(m) -> str:
-     "Returns a string"
+    "Returns a string"
+
 
 @mod.capture
 def task_modifiable_verbs(m) -> str:
-     "Returns a string"
+    "Returns a string"
+
 
 @mod.capture
 def task_modifiers(m) -> str:
-     "Returns a string"
+    "Returns a string"
 
-@ctx.capture(rule='{self.task_unmodifiable_verbs}')
+
+@ctx.capture(rule="{self.task_unmodifiable_verbs}")
 def task_unmodifiable_verbs(m) -> str:
     "Returns a string"
     return m.task_unmodifiable_verbs
 
-@ctx.capture(rule='{self.task_modifiable_verbs}')
+
+@ctx.capture(rule="{self.task_modifiable_verbs}")
 def task_modifiable_verbs(m) -> str:
     "Returns a string"
     return m.task_modifiable_verbs
 
-@ctx.capture(rule='{self.task_modifiers}')
+
+@ctx.capture(rule="{self.task_modifiers}")
 def task_modifiers(m) -> str:
     "Returns a string"
     return m.task_modifiers
 
-#@ctx.capture(rule='{self.task_command}')
-#def task_command(m) -> str:
+
+# @ctx.capture(rule='{self.task_command}')
+# def task_command(m) -> str:
 #    "Returns a string"
 #    return m.task_command
