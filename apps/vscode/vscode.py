c0ba5bd26e1e50965902a28a86197184c64a9844
Aaron Adams <aaron.adams@nccgroup.com>
2020-06-12 14:20:24 +0800

    auto pep8 stuff

--- apps/vscode/vscode.py
+++ apps/vscode/vscode.py
@@ -1,22 +1,25 @@
-from talon import Context, actions, ui, Module, app
-is_mac = app.platform == 'mac'
+from talon import Context, Module, actions, app, ui
+
+is_mac = app.platform == "mac"
 
 ctx = Context()
-ctx.matches = r'''
+ctx.matches = r"""
 app: Code
 app: Code - OSS
 app: Code
 app: Visual Studio Code
 app: Code.exe
-'''
-@ctx.action_class('win')
+"""
+
+
+@ctx.action_class("win")
 class win_actions:
-    def filename(): 
+    def filename():
         title = actions.win.title()
-        #this doesn't seem to be necessary on VSCode for Mac
-        #if title == "":
+        # this doesn't seem to be necessary on VSCode for Mac
+        # if title == "":
         #    title = ui.active_window().doc
-    
+
         if is_mac:
             result = title.split(" — ")[0]
         else:
@@ -24,7 +27,7 @@ class win_actions:
 
         if "." in result:
             return result
-    
+
         return ""
 
     def file_ext():
