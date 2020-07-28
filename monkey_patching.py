0d0c4730bbff0d51f9c9ddf196b936def6506a08
Aaron Adams <aaron.adams@nccgroup.com>
2020-07-28 11:58:25 +0800

    monkey patch for i3wm focusing

--- monkey_patching.py
+++ monkey_patching.py
@@ -1,12 +1,16 @@
 import os
 
-from talon import app
+from talon import app, ui
 
 
-def monkey_notify(body, title, subtitle, *kwargs):
-    # os.system('notify-send.sh -t 3000 -f -u low "Talon Sleep"')
+def monkey_notify(body="", title="", subtitle="", *kwargs):
     print(kwargs)
     os.system('notify-send.sh -t 3000 -f -u low "%s"' % subtitle)
 
 
+def monkey_focus(self, *kwargs):
+    os.system("i3-msg '[class=\"(?)%s\"] focus'" % self.name)
+
+
 app.notify = monkey_notify
+ui.App.focus = monkey_focus
