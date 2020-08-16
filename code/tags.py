2537a2adccac930156df0519f92beb31300afcf3
Aaron Adams <aaron.adams@nccgroup.com>
2020-08-16 17:47:47 +0800

    mode stuff moved to mode.py

--- code/tags.py
+++ code/tags.py
@@ -12,16 +12,6 @@ tagList = [
     "disassembler",
     "ida",
 ]
-modes = {
-    "gdb": "a way to force gdb commands to be loaded",
-    "windbg": "a way to force windbg commands to be loaded",
-    "ida": "a way to force ida commands to be loaded",
-    "debug": "a way to force debugger commands to be loaded",
-    "admin": "enable extra administration commands terminal (docker, etc)",
-}
-
 for entry in tagList:
     mod.tag(entry, f"tag to load {entry} and/or related plugins ")
 
-for key, value in modes.items():
-    mod.mode(key, value)
